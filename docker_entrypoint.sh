ebeca33bc94a1331ce9e8398b5aabb889da571e5
Ks2wAu89jnEgCR0bGUAA <github.com.oversleep470@passmail.com>
2025-06-09 10:47:28 +0100

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -7,10 +7,8 @@ export EMBASSY_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 export PEER_TOR_ADDRESS=$(yq e '.peer-tor-address' "$CONFIG_FILE")
 export RPC_TOR_ADDRESS=$(yq e '.rpc-tor-address' "$CONFIG_FILE")
 
-# Start Flask UI in background
-echo "Starting bitcoin Stats Monitoring app..."
-python3 /app/bitcoin-stats.py &
+# Start bitcoind-manager in background
+bitcoind-manager &
 
-# Start the Bitcoin manager (main process)
-echo "Starting bitcoind-manager..."
-exec tini -p SIGTERM -- bitcoind-manager
+# Start the Flask app on port 5006
+python3 /opt/app/bitcoin-stats.py
