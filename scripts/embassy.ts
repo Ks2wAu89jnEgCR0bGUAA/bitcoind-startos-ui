db0ff53a99f9a4d003ba17afec2a3e0185cdca2f
Ks2wAu89jnEgCR0bGUAA <github.com.oversleep470@passmail.com>
2025-06-11 07:22:40 +0100

    Update embassy.ts

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,7 +1,7 @@
 // @ts-check
 export { setConfig } from "./services/setConfig.ts";
 export { properties } from "./services/properties.ts";
-export { health } from "./services/healthChecks.ts";
+export { healthChecks } from "./services/healthChecks.ts";
 export { getConfig } from "./services/getConfig.ts";
 export { action } from "./services/action.ts";
 export { migration } from "./services/migrations.ts";
